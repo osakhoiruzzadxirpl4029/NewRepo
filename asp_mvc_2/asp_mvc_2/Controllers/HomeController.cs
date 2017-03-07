4f99bb11b28c67f0bace41ce05c73f6d1fa78234
osakhoiruzzadxirpl4029 <khoiruzzad72@gmail.com>
2017-03-07 22:36:12 +0700

    A

---
+++ asp_mvc_2/asp_mvc_2/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using asp_mvc_2.Security;
+using System.Web.Security;
+
+namespace asp_mvc_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+        [AuthorizeRoles("Admin")]
+        public ActionResult AdminOnly() {  
+       return View();
+        }
+
+        public ActionResult UnAuthorized() {   
+       return View();
+        }
+
+
+    }
+}
